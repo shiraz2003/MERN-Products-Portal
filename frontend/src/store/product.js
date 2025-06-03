360341b5e920ee87a1d98429a37772832399e5cf
shiraz2003 <ssappideen@yahoo.com>
2025-06-03 23:28:02 +0530

    add product store using zustand for state management

---
+++ frontend/src/store/product.js
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+export const useProductStore = create((set) => ({
+    products: [],
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.image || !newProduct.price) {
+            return { success: false, message: "please fill all fields" }
+
+        }
+        const res= await fetch("/api/products")
+
+    }
+
+
+}));
