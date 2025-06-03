164a1c2aa24652cdb3a472d280e707f0f8eeb9a2
shiraz2003 <ssappideen@yahoo.com>
2025-06-03 23:25:57 +0530

    added products model and modified the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import { connect } from 'mongoose';
-import Product from './models/producst.model.js';
+import routes from './routes/routes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/products', async (req, res) => {
-const product = req.body; //user will send data 
-if(!product.name||!product.price||!product.image){
-    return res.status(400).json({message:'All fields are required'});
-}
-
-const newProduct = new Product(product);
-
-try{
-    await newProduct.save();
-    res.status(201).json(newProduct);
-}catch(error){
-    console.error(error);
-    res.status(500).json({message:'Server Error'});
-}
-
-});
-
+// Use the product routes
+app.use('/api/products', routes);
 
 app.listen(5000, () => {
     connectDB();
